238237e3881509a8717bec5a8f4b62fbdaba5c17
Prakhar Shrivastava <prakharshrivastava971@gmail.com>
2023-03-31 13:20:36 +0530

    Row Color changing logic added

--- src/pages/Program/index.jsx
+++ src/pages/Program/index.jsx
@@ -1,6 +1,32 @@
 import React from "react";
 import { useLocation, useParams } from "react-router-dom";
 
+const StylesConfig = {
+  default: {
+    styles: "bg-white",
+    color: "grey",
+  },
+  completed: {
+    styles: "bg-green-500 text-white",
+    color: "white",
+  },
+  late: {
+    styles: "bg-yellow-500 text-white",
+    color: "white",
+  },
+  pending: {
+    styles: "bg-red-500 text-white",
+    color: "white",
+  },
+  blocked: {
+    styles: "bg-gray-500 text-white",
+    color: "white",
+  },
+  advanced: {
+    styles: "bg-blue-500 text-white",
+    color: "white",
+  },
+};
 const Program = () => {
   const location = useLocation();
   const program = location?.state?.program;
@@ -22,16 +48,22 @@ const Program = () => {
           </tr>
         </thead>
         <tbody className="text-center">
-          {program?.enrolledUsers?.map((user) => (
-            <tr key={user?._id} className="border-b">
-              <td className="py-2">{user?.userid?.username}</td>
-              <td>{user?.userid?.email}</td>
-              <td>5</td>
-              <td>5</td>
-              <td>5</td>
-              <td>{user?.completedPercentage}%</td>
-            </tr>
-          ))}
+          {program?.enrolledUsers?.map((user) => {
+            const classes =
+              StylesConfig[user?.currentPerformance]?.styles || "";
+            const color =
+              StylesConfig[user?.currentPerformance]?.color || "white";
+            return (
+              <tr key={user?._id} className={`${classes}`}>
+                <td className="py-2">{user?.userid?.username}</td>
+                <td>{user?.userid?.email}</td>
+                <td>{user?.completedChallengesCount}</td>
+                <td>0</td>
+                <td>{user?.completedAssignmentCount}</td>
+                <td>{user?.completedPercentage}%</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
