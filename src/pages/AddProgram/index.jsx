b59bc938ca5ea9ef4fb7c4ef2b184d262155cbc6
Prakhar Shrivastava <prakharshrivastava971@gmail.com>
2023-06-07 21:48:34 +0530

    commtedMins added in UI

--- src/pages/AddProgram/index.jsx
+++ src/pages/AddProgram/index.jsx
@@ -12,11 +12,13 @@ export const AddProgram = () => {
     const programName = formData.get("programName");
     const learningPath = formData.get("learningPath");
     const emails = formData.get("emails");
+    const commitedMins = formData.get("commitedMins");
+
     setLoading(true);
     addProgram({
       programName,
       learningPath,
-      emails,
+      emails,commitedMins
     })
       .then((res) => {
         if (res.status === 200) {
@@ -84,6 +86,23 @@ export const AddProgram = () => {
             <p className="text-white text-xs italic">
               Please enter emails of learners as a comma seperated value
             </p>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-white text-xs font-bold mb-2"
+              for="grid-password"
+            >
+              Commited Minutes
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              name="commitedMins"
+              type="number"
+              placeholder="Commited Minutes"
+            />
           </div>
         </div>
 
