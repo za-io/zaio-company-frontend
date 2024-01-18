88b79222edfa92f7da421fe3270aa4baef519bb7
Prakhar Shrivastava <prakharshrivastava971@gmail.com>
2024-01-18 20:47:14 +0530

    backend url updated

--- src/api/student.js
+++ src/api/student.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  // process.env.REACT_APP_BACKEND_URL +
-
-  "http://localhost:4000/student-analytics";
+  process.env.REACT_APP_BACKEND_URL + "/student-analytics";
 const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 export const getUserBootcampAnalytics = (user_id, bootcamp_id) =>
